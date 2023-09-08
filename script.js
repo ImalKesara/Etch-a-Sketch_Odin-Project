a45531652d6d8506047ceed4d08dce2d1ca64e66
W.I.K.N.Jayathilaka <imalkesara2001@gmail.com>
2023-09-09 00:29:25 +0530

    add erase btn

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 let form = document.querySelector('.form');
 let userinput = document.querySelector('.userinput');
 const grid = document.querySelector('.grid');
+const erase = document.querySelector('button');
 let userTotal;
 
 
@@ -12,31 +13,30 @@ form.addEventListener('submit', (e) => {
     console.log(userTotal);
 
     for (let i = 0; i < userTotal; i++) {
-        // let newDiv = document.createElement('div');
-        // newDiv.setAttribute('class', 'grid');
-        // console.log(newDiv);
-        // wrapper.appendChild(newDiv);
         console.log('four');
         let newDiv = document.createElement('div');
         newDiv.setAttribute('class', 'grid');
         wrapper.appendChild(newDiv);
         newDiv.style.width = 500 / userinput.value + 'px';
         newDiv.style.height = 500 / userinput.value + 'px';
-        newDiv.style.backgroundColor = 'pink';
+        newDiv.style.backgroundColor = 'white';
         newDiv.style.border = '1px solid aqua';
 
         if (userinput.value > 64) {
             break;
         }
-        newDiv.addEventListener('contextmenu', function(E) {
+        newDiv.addEventListener('click', function(E) {
             console.log(this)
             this.style.backgroundColor = 'red';
         })
 
-        newDiv.addEventListener('click', function(E) {
-            console.log(this)
-            this.style.backgroundColor = 'pink';
+        erase.addEventListener('click', () => {
+            newDiv.addEventListener('mouseover', function(E) {
+                console.log(this)
+                this.style.backgroundColor = 'white';
+            })
         })
+
 
 
 
